66ead09bdb0691f8f0d12b44e4a1362cc7fbe342
Wenxuan <wenxuangm@gmail.com>
2018-04-07 12:01:48 +0800

    Init commit

---
+++ chrome-bookmarks-parser.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'json'
+FILE = ARGV.first
+CJK  = /\p{Han}|\p{Katakana}|\p{Hiragana}|\p{Hangul}/
+
+def build parent, json
+  name = [parent, json['name']].compact.join('/')
+  if json['type'] == 'folder'
+    json['children'].map { |child| build name, child }
+  else
+    { name: name, url: json['url'] }
+  end
+end
+
+def just str, width
+  str.ljust(width - str.scan(CJK).length)
+end
+
+def trim str, width
+  len = 0
+  str.each_char.each_with_index do |char, idx|
+    len += char =~ CJK ? 2 : 1
+    return str[0, idx] if len > width
+  end
+  str
+end
+
+width = `tput cols`.strip.to_i / 2
+json  = JSON.load File.read File.expand_path FILE
+items = json['roots']
+        .values_at(*%w(bookmark_bar synced other))
+        .compact
+        .map { |e| build nil, e }
+        .flatten
+
+items.each do |item|
+  name = trim item[:name], width
+  puts [just(name, width),
+        item[:url]].join("\t\x1b[36m") + "\x1b[m"
+end
